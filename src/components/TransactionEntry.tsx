063cd635dbfe63f4541643100b95a35d202748e8
Mark Gabriel H. Dela Rosa <mark2231delarosa@gmail.com>
2025-11-17 20:37:48 +0800

    feat(transaction-entry): add edit/delete buttons and styling
    - Added non-functional buttons for Update and Delete actions
    - Applied styling to transaction entry sections

--- src/components/TransactionEntry.tsx
+++ src/components/TransactionEntry.tsx
@@ -1,4 +1,7 @@
 import type { Entry } from "src/types";
+import '@styles/taskEntry.scss';
+import DeleteIcon from '@assets/icons/delete-icon.svg';
+import EditIcon from '@assets/icons/edit-icon.svg'
 
 export default function TransactionEntry({ type, title, category, amount, date }: Entry) {
     function formatAmount(type: string, amount: number) {
@@ -12,22 +15,37 @@ export default function TransactionEntry({ type, title, category, amount, date }
 
     return (
         <article className="transaction-entry">
-            <header className="transaction-entry__header">
-                <span>{category}</span>
-                <time dateTime={date}>{new Date(date).toLocaleTimeString('en-PH', {
-                    hour: 'numeric',
-                    minute: '2-digit',
-                    hour12: true,
-                })}</time>
-            </header>
-            <div className="transaction-entry__body">
-                <h3>{title}</h3>
-                <p aria-label={`transaction amount: ${type === 'income' ? amount : `-${amount}`}`}>
-                    <data value={amount}>
-                        {formatAmount(type, amount)}
-                    </data>
-                </p>
-            </div>
+            <section className="transaction-entry__section transaction-entry__overview">
+                <header className="transaction-entry__section__header">
+                    <span>{category}</span>
+                    <time dateTime={date}>{new Date(date).toLocaleTimeString('en-PH', {
+                        hour: 'numeric',
+                        minute: '2-digit',
+                        hour12: true,
+                    })}</time>
+                </header>
+                <div className="transaction-entry__section__header">
+                    <h3>{title}</h3>
+                    <p data-role={`transaction-amount__${type}`} aria-label={`transaction amount: ${type === 'income' ? amount : `-${amount}`}`}>
+                        <data value={amount}>
+                            {formatAmount(type, amount)}
+                        </data>
+                    </p>
+                </div>
+            </section>
+            <section className="transaction-entry__section transaction-entry__actions">
+                <button
+                    className="transaction-entry__button transaction-entry__button--edit"
+                    aria-label="Edit transaction"
+                >
+                    <img src={EditIcon} alt="Edit transaction" aria-hidden="true" />
+                </button>
+                <button className="transaction-entry__button transaction-entry__button--delete"
+                    aria-label="Delete transaction"
+                >
+                    <img src={DeleteIcon} alt="Delete transaction" aria-hidden="true" />
+                </button>
+            </section>
         </article>
     )
 }
